b05feee5895101d14c93acdcb8c645d15bcb9adb
Jimmy Velasco <jvelas00@g.uafs.edu>
2021-09-13 15:52:33 +0000

    added files

---
+++ bool.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+	unsigned char value1 = 0x80;
+	unsigned char value2 = 0x20;
+
+unsigned char result = value1 + value2;
+
+	printf("[%x]value1: %d 0x%x\n", value1,value1,value1);
+	printf("[%x]value2: %d 0x%x\n", value2,value2,value2);
+	printf("[%x]result: %d 0x%x\n", result,result,result);
+
+	return 0;
+}
